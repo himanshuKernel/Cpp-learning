7dc3588be975cf97af354976c27403ca8f846f2f
Himanshu Kumar <himansh.notes@gmail.com>
2026-08-04 23:27:32 +0530

    Add c++ fudamental programs

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int score;
+    score = 110;
+
+    const int uid = 232323;
+
+    int himanshuBalance = 500;
+    himanshuBalance = 1000;
+
+    // uid = 1223;
+    cout << "Welcome to chai with cpp 1" << endl;
+
+    cout << "Welcome to chai with cpp 2" << endl;
+    cout << "Welcome to chai with cpp 3" << endl;
+    return 0;
+}
