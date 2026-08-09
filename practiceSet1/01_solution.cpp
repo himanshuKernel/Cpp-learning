be9b108492fbf33255223bdf924a14d536c67bda
Himanshu Kumar <himansh.notes@gmail.com>
2026-08-09 17:31:43 +0530

    Learned about different types of operators in C++

--- practiceSet1/01_solution.cpp
+++ practiceSet1/01_solution.cpp
@@ -6,12 +6,12 @@ int main(){
     string teaType = "Masala tea";
     float pricePerkg = 699.25;
     char rating = 'A';
-
+    
     cout <<"Tea Information\n";
     cout <<"------------------------\n";
     cout <<"Tea Type   : \""<<teaType<<"\"\n";
     cout <<"Price Per Kg : Rs."<<pricePerkg<<"\n";
     cout <<"Rating    : "<<rating<<"\n";
-
+    
     return 0;
 }
